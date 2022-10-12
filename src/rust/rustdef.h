54b600d23c42d337d1ff7858b1cac4e7c7d4f4b3
yanglinshu <nicolasvandarkholme@outlook.com>
2022-10-12 16:01:22 +0800

    Fix rustdef

--- src/rust/rustdef.h
+++ src/rust/rustdef.h
@@ -24,20 +24,24 @@
 
 #define panic_(x) throw(x)
 
-using i8  = int8_t;
-using i16 = int16_t;
-using i32 = int32_t;
-using i64 = int64_t;
+#define i8  signed char
+#define i16 short
+#define i32 int
+#define i64 long long
 
-using u8  = uint8_t;
-using u16 = uint16_t;
-using u32 = uint32_t;
-using u64 = uint64_t;
+#define u8  unsigned char
+#define u16 unsigned short
+#define u32 unsigned int
+#define u64 unsigned long long
 
-using isize = intptr_t;
-using usize = uintptr_t;
+#define isize intptr_t
+#define usize size_t
 
-using f32 = float;
-using f64 = double;
+#define f32 float
+#define f64 double
+
+#define bool bool
+
+#define char unsigned char
 
 #endif
